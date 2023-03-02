4b8e1a039229ce8637385bb0ad83ab298e8cc780
unknown <gauravrajanju@gmail.com>
2023-03-03 02:24:29 +0530

    animation

--- clipper_curve_animation/lib/home_page.dart
+++ clipper_curve_animation/lib/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'dart:math' show pi;
 import 'home_page_helper.dart';
+import 'dart:math' show pi;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -19,33 +19,34 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
-    _animationController = AnimationController(vsync: this, duration: const Duration(seconds: 1));
-    _animation = Tween<double>(begin: 0, end: -pi / 2).animate(CurvedAnimation(parent: _animationController, curve: Curves.bounceOut));
-    _animationController.forward();
+    // _animationController = AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    // _animation = Tween<double>(begin: 0, end: -pi / 2).animate(CurvedAnimation(parent: _animationController, curve: Curves.bounceOut));
+    // _animationController.forward();
 
-    _flipController = AnimationController(vsync: this, duration: const Duration(seconds: 1));
-    // _flipAnimation = Tween<double>(begin: 0, end: 0).animate(
-    //   CurvedAnimation(parent: _flipController, curve: Curves.bounceOut),
-    // );
+    _flipController = AnimationController(vsync: this, duration: const Duration(seconds: 3));
+    _flipAnimation = Tween<double>(begin: 0, end: 2 * pi).animate(
+      CurvedAnimation(parent: _flipController, curve: Curves.easeInOutCubic),
+    );
+    _flipController.repeat();
 
-    if (_animationController.value == 1.0 && _flipController.value == 0.0) {
-      print(_animation.value);
-      _animationController.reset();
-      _flipController.forward();
-      _flipAnimation = Tween<double>(begin: _flipAnimation?.value ?? 0, end: _flipAnimation?.value ?? 0 + pi).animate(
-        CurvedAnimation(parent: _flipController, curve: Curves.bounceOut),
-      );
-    }
+    // if (_animationController.value == 1.0 && _flipController.value == 0.0) {
+    //   print(_animation.value);
+    //   _animationController.reset();
+    //   _flipController.forward();
+    //   _flipAnimation = Tween<double>(begin: _flipAnimation?.value ?? 0, end: _flipAnimation?.value ?? 0 + pi).animate(
+    //     CurvedAnimation(parent: _flipController, curve: Curves.bounceOut),
+    //   );
+    // }
 
-    if (_flipController.value == 1.0 && _animationController.value == 0.0) {
-      print(_flipAnimation?.value);
-      print(_animation.value);
-      _flipController.reset();
-      _animationController.forward();
-      _animation = Tween<double>(begin: _animation.value, end: _animation.value + -pi / 2).animate(
-        CurvedAnimation(parent: _animationController, curve: Curves.bounceOut),
-      );
-    }
+    // if (_flipController.value == 1.0 && _animationController.value == 0.0) {
+    //   print(_flipAnimation?.value);
+    //   print(_animation.value);
+    //   _flipController.reset();
+    //   _animationController.forward();
+    //   _animation = Tween<double>(begin: _animation.value, end: _animation.value + -pi / 2).animate(
+    //     CurvedAnimation(parent: _animationController, curve: Curves.bounceOut),
+    //   );
+    // }
   }
 
   @override
@@ -61,48 +62,40 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
       child: Scaffold(
         body: Center(
           child: AnimatedBuilder(
-              animation: _animationController,
+              animation: _flipController,
               builder: (context, child) {
-                return AnimatedBuilder(
-                    animation: _flipController,
-                    builder: (context, child) {
-                      return Transform(
-                        alignment: Alignment.center,
-                        transform: Matrix4.identity()..rotateZ(_animation.value),
-                        child: Transform(
-                          alignment: Alignment.center,
-                          transform: Matrix4.identity()..rotateY(_flipAnimation?.value ?? 0),
-                          child: Stack(
-                            alignment: Alignment.center,
-                            children: [
-                              ClipPath(
-                                clipper: CircleCustomClipper(),
-                                child: Container(
-                                  height: 350,
-                                  width: 350,
-                                  color: Colors.yellowAccent,
-                                ),
-                              ),
-                              ClipPath(
-                                clipper: CircleCustomClipper(),
-                                child: Container(
-                                  height: 300,
-                                  width: 300,
-                                  color: const Color.fromARGB(255, 204, 184, 7),
-                                ),
-                              ),
-                              CustomPaint(
-                                painter: SymbolCustomPainter(),
-                                child: const SizedBox(
-                                  height: 400,
-                                  width: 400,
-                                ),
-                              ),
-                            ],
-                          ),
+                return Transform(
+                  alignment: Alignment.center,
+                  transform: Matrix4.identity()..rotateY(_flipAnimation?.value ?? 0),
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      ClipPath(
+                        clipper: CircleCustomClipper(),
+                        child: Container(
+                          height: 350,
+                          width: 350,
+                          color: Colors.yellowAccent,
                         ),
-                      );
-                    });
+                      ),
+                      ClipPath(
+                        clipper: CircleCustomClipper(),
+                        child: Container(
+                          height: 300,
+                          width: 300,
+                          color: const Color.fromARGB(255, 204, 184, 7),
+                        ),
+                      ),
+                      CustomPaint(
+                        painter: SymbolCustomPainter(),
+                        child: const SizedBox(
+                          height: 400,
+                          width: 400,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
               }),
         ),
       ),
